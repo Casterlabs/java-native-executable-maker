83103ff44615a54beb7be758dd0f8e9d8ec9d42b
Ender <33337309+e3ndr@users.noreply.github.com>
2023-07-02 20:39:03 -0500

    Update build.sh

--- src/main/c/build.sh
+++ src/main/c/build.sh
@@ -6,11 +6,20 @@ mkdir dist
 # ----------------------------------------- #
 #                  Windows                  #
 # ----------------------------------------- #
-zig build-exe -target x86_64-windows-gnu -lshlwapi --subsystem windows -lc --name intermediate __windows.c tiny-json.c
+
+zig build-exe -target x86-windows -lshlwapi --subsystem windows -lc --name intermediate __windows.c tiny-json.c
+mv intermediate.exe dist/Windows-x86.exe
+mv intermediate.pdb dist/Windows-x86.pdb
+
+zig build-exe -target x86_64-windows -lshlwapi --subsystem windows -lc --name intermediate __windows.c tiny-json.c
 mv intermediate.exe dist/Windows-x86_64.exe
 mv intermediate.pdb dist/Windows-x86_64.pdb
 
-zig build-exe -target aarch64-windows-gnu -lshlwapi --subsystem windows -lc --name intermediate __windows.c tiny-json.c
+# zig build-exe -target arm-windows -lshlwapi --subsystem windows -lc --name intermediate __windows.c tiny-json.c
+# mv intermediate.exe dist/Windows-arm.exe
+# mv intermediate.pdb dist/Windows-arm.pdb
+
+zig build-exe -target aarch64-windows -lshlwapi --subsystem windows -lc --name intermediate __windows.c tiny-json.c
 mv intermediate.exe dist/Windows-aarch64.exe
 mv intermediate.pdb dist/Windows-aarch64.pdb
 
